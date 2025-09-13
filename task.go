989f6ea513b32609162d81c98a2866b5fcb0656c
Rangel Reale <rangelspam@gmail.com>
2025-09-13 15:55:43 -0300

    new test

--- task.go
+++ task.go
@@ -189,11 +189,13 @@ type serviceWithCallback struct {
 var _ Service = (*serviceWithCallback)(nil)
 
 func (s *serviceWithCallback) Start(ctx context.Context) error {
-	return s.svc.Start(ctx)
+	return errors.New("this should never run")
+	// return s.svc.Start(ctx)
 }
 
 func (s *serviceWithCallback) Stop(ctx context.Context) error {
-	return s.svc.Stop(ctx)
+	return errors.New("this should never run")
+	// return s.svc.Stop(ctx)
 }
 
 func (s *serviceWithCallback) ToTask(isStart bool) (tt Task) {
