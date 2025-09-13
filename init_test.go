9c970b26602219d019c9cf9353034a39d891fa94
Rangel Reale <rangelspam@gmail.com>
2025-09-13 09:01:44 -0300

    callback

--- init_test.go
+++ init_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"gotest.tools/v3/assert"
+	cmp2 "gotest.tools/v3/assert/cmp"
 )
 
 type testTaskNoError struct {
@@ -271,6 +272,12 @@ func TestSvcInitCallback(t *testing.T) {
 			stdAdd = 2
 		}
 		return TaskCallbackFunc(func(ctx context.Context, task Task) {
+			if tn, ok := task.(*testTask); ok {
+				assert.Check(t, cmp2.Equal(tn.taskNo, taskNo))
+			} else {
+				assert.Check(t, false, "task is not of the expected type")
+			}
+
 			if !isStop {
 				started.add((taskNo * 10) + stdAdd)
 			} else {
@@ -286,24 +293,24 @@ func TestSvcInitCallback(t *testing.T) {
 	}
 
 	stopTask1 := sinit.
-		StartTask(TaskFuncWithCallback(func(ctx context.Context) error {
+		StartTask(TaskWithCallback(newTestTask(1, func(ctx context.Context) error {
 			started.add(1)
 			return nil
-		}, getTaskCallback(1, false))).
-		ManualStop(TaskFuncWithCallback(func(ctx context.Context) error {
+		}), getTaskCallback(1, false))).
+		ManualStop(TaskWithCallback(newTestTask(1, func(ctx context.Context) error {
 			stopped.add(1)
 			return nil
-		}, getTaskCallback(1, true)))
+		}), getTaskCallback(1, true)))
 
 	stopTask2 := sinit.
 		StartTaskFunc(func(ctx context.Context) error {
 			started.add(2)
 			return nil
 		}).
-		ManualStop(TaskFuncWithCallback(func(ctx context.Context) error {
+		ManualStop(TaskWithCallback(newTestTask(2, func(ctx context.Context) error {
 			stopped.add(2)
 			return nil
-		}, getTaskCallback(2, true)))
+		}), getTaskCallback(2, true)))
 
 	sinit.StopTask(stopTask1)
 	sinit.StopTask(stopTask2)
@@ -368,6 +375,22 @@ func TestSvcInitPendingStopService(t *testing.T) {
 
 	err := sinit.Run()
 	assert.ErrorIs(t, err, ErrPending)
+}
+
+type testTask struct {
+	taskNo int
+	task   TaskFunc
+}
+
+func newTestTask(taskNo int, task TaskFunc) *testTask {
+	return &testTask{
+		taskNo: taskNo,
+		task:   task,
+	}
+}
+
+func (t *testTask) Run(ctx context.Context) error {
+	return t.task(ctx)
 }
 
 type testList[T any] struct {
