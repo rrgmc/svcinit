a79d3c5e8b968f8f50bf55d9d200b23ade2bbfaf
Rangel Reale <rangelspam@gmail.com>
2025-09-18 05:07:25 -0300

    refactor

--- util.go
+++ util.go
@@ -2,6 +2,7 @@ package svcinit
 
 import (
 	"context"
+	"errors"
 	"slices"
 	"sync"
 )
@@ -77,6 +78,26 @@ func waitGroupWaitWithContext(ctx context.Context, wg *sync.WaitGroup) bool {
 	case <-ctx.Done():
 		return false // timed out
 	}
+}
+
+type multiError struct {
+	Errors []error
+}
+
+func (e *multiError) Error() string {
+	err := e.Err()
+	if err == nil {
+		return "empty errors"
+	}
+	return err.Error()
+}
+
+func (e *multiError) Err() error {
+	return errors.Join(e.Errors...)
+}
+
+func (e *multiError) Unwrap() []error {
+	return e.Errors
 }
 
 type multiErrorBuilder struct {
