ea4ccc8504f1282231d7ef92d1d34ea546cde9ad
Rangel Reale <rangelspam@gmail.com>
2025-09-22 08:12:53 -0300

    wrap

--- wrap.go
+++ wrap.go
@@ -8,12 +8,12 @@ import (
 // TaskWithWrapped is a task which was wrapped from another Task.
 type TaskWithWrapped interface {
 	Task
-	TaskWrapped() Task
+	WrappedTask() Task
 }
 
-// NewWrappedTask wraps a task in a TaskWithWrapped, allowing the handler to be customized.
-func NewWrappedTask(task Task, options ...WrapTaskOption) *WrappedTask {
-	ret := &WrappedTask{
+// WrapTask wraps a task in a TaskWithWrapped, allowing the handler to be customized.
+func WrapTask(task Task, options ...WrapTaskOption) TaskWithWrapped {
+	ret := &wrappedTask{
 		task: task,
 	}
 	for _, option := range options {
@@ -22,18 +22,18 @@ func NewWrappedTask(task Task, options ...WrapTaskOption) *WrappedTask {
 	return ret
 }
 
-type WrapTaskOption func(task *WrappedTask)
+type WrapTaskOption func(task *wrappedTask)
 
 // WithWrapTaskHandler sets an optional handler for the task.
 func WithWrapTaskHandler(handler TaskHandler) WrapTaskOption {
-	return func(task *WrappedTask) {
+	return func(task *wrappedTask) {
 		task.handler = handler
 	}
 }
 
 // WithWrapDescription sets the task description.
 func WithWrapDescription(description string) WrapTaskOption {
-	return func(task *WrappedTask) {
+	return func(task *wrappedTask) {
 		task.description = description
 	}
 }
@@ -44,53 +44,53 @@ func UnwrapTask(task Task) Task {
 		return nil
 	}
 	for {
-		if tc, ok := task.(TaskWithWrapped); ok && tc.TaskWrapped() != nil {
-			task = tc.TaskWrapped()
+		if tc, ok := task.(TaskWithWrapped); ok && tc.WrappedTask() != nil {
+			task = tc.WrappedTask()
 		} else {
 			return task
 		}
 	}
 }
 
-type WrappedTask struct {
+type wrappedTask struct {
 	task        Task
 	handler     TaskHandler
 	description string
 }
 
-var _ Task = (*WrappedTask)(nil)
-var _ TaskSteps = (*WrappedTask)(nil)
-var _ TaskWithOptions = (*WrappedTask)(nil)
-var _ TaskWithWrapped = (*WrappedTask)(nil)
+var _ Task = (*wrappedTask)(nil)
+var _ TaskSteps = (*wrappedTask)(nil)
+var _ TaskWithOptions = (*wrappedTask)(nil)
+var _ TaskWithWrapped = (*wrappedTask)(nil)
 
-func (t *WrappedTask) Run(ctx context.Context, step Step) error {
+func (t *wrappedTask) Run(ctx context.Context, step Step) error {
 	if t.handler != nil {
 		return t.handler(ctx, t.task, step)
 	}
 	return t.task.Run(ctx, step)
 }
 
-func (t *WrappedTask) TaskOptions() []TaskInstanceOption {
+func (t *wrappedTask) TaskOptions() []TaskInstanceOption {
 	if tt, ok := t.task.(TaskWithOptions); ok {
 		return tt.TaskOptions()
 	}
 	return nil
 }
 
-func (t *WrappedTask) TaskSteps() []Step {
+func (t *wrappedTask) TaskSteps() []Step {
 	if tt, ok := t.task.(TaskSteps); ok {
 		return tt.TaskSteps()
 	}
 	return DefaultTaskSteps()
 }
 
-func (t *WrappedTask) TaskWrapped() Task {
+func (t *wrappedTask) WrappedTask() Task {
 	return t.task
 }
 
-func (t *WrappedTask) String() string {
+func (t *wrappedTask) String() string {
 	if t.description != "" {
 		return t.description
 	}
-	return fmt.Sprintf("WrappedTask(%v)", t.task)
+	return fmt.Sprintf("wrappedTask(%v)", t.task)
 }
