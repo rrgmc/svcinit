c874c543cfcf10b886f9ab6016d3f73434806f59
Rangel Reale <rangelspam@gmail.com>
2025-09-24 12:24:03 -0300

    build parent

--- build.go
+++ build.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"maps"
 	"slices"
+	"strings"
 )
 
 type TaskBuildFunc func(ctx context.Context) error
@@ -14,6 +15,12 @@ func BuildTask(options ...TaskBuildOption) Task {
 }
 
 type TaskBuildOption func(*taskBuild)
+
+func WithParent(parent Task) TaskBuildOption {
+	return func(build *taskBuild) {
+		build.parent = parent
+	}
+}
 
 func WithDescription(description string) TaskBuildOption {
 	return func(build *taskBuild) {
@@ -55,14 +62,17 @@ func WithTaskOptions(options ...TaskInstanceOption) TaskBuildOption {
 
 type taskBuild struct {
 	stepFunc    map[Step]TaskBuildFunc
+	parent      Task
 	steps       []Step
 	options     []TaskInstanceOption
+	initError   error
 	description string
 }
 
 var _ Task = (*taskBuild)(nil)
 var _ TaskSteps = (*taskBuild)(nil)
 var _ TaskWithOptions = (*taskBuild)(nil)
+var _ TaskWithInitError = (*taskBuild)(nil)
 
 func newTaskBuild(options ...TaskBuildOption) Task {
 	ret := &taskBuild{
@@ -71,7 +81,10 @@ func newTaskBuild(options ...TaskBuildOption) Task {
 	for _, opt := range options {
 		opt(ret)
 	}
-	ret.init()
+	err := ret.init()
+	if err != nil {
+		ret.initError = err
+	}
 	if ret.isEmpty() {
 		return nil
 	}
@@ -86,9 +99,24 @@ func (t *taskBuild) TaskOptions() []TaskInstanceOption {
 	return t.options
 }
 
+func (t *taskBuild) TaskInitError() error {
+	return t.initError
+}
+
 func (t *taskBuild) Run(ctx context.Context, step Step) error {
+	var parentHasStep bool
+	if t.parent != nil {
+		parentHasStep = taskHasStep(t.parent, step)
+	}
+
 	if fn, ok := t.stepFunc[step]; ok {
+		if parentHasStep {
+			return fmt.Errorf("%w: build task parent already has '%s' step", ErrDuplicateStep, step.String())
+		}
 		return fn(ctx)
+	}
+	if parentHasStep {
+		return t.parent.Run(ctx, step)
 	}
 	return newInvalidTaskStep(step)
 }
@@ -96,6 +124,9 @@ func (t *taskBuild) Run(ctx context.Context, step Step) error {
 func (t *taskBuild) String() string {
 	if t.description != "" {
 		return t.description
+	}
+	if t.parent != nil {
+		return taskDescription(t.parent)
 	}
 	return fmt.Sprintf("%T", t)
 }
@@ -112,6 +143,27 @@ func (t *taskBuild) isEmpty() bool {
 	return false
 }
 
-func (t *taskBuild) init() {
+func (t *taskBuild) init() error {
+	var duplicatedSteps []Step
+
 	t.steps = slices.Collect(maps.Keys(t.stepFunc))
+
+	if t.parent != nil {
+		for _, step := range taskSteps(t.parent) {
+			if !slices.Contains(t.steps, step) {
+				t.steps = append(t.steps, step)
+			} else {
+				duplicatedSteps = append(duplicatedSteps, step)
+			}
+		}
+	}
+
+	if len(duplicatedSteps) > 0 {
+		return fmt.Errorf("%w: build task parent already has '%s' step(s)", ErrDuplicateStep,
+			strings.Join(sliceMap(duplicatedSteps, func(i int, e Step) string {
+				return e.String()
+			}), ","))
+	}
+
+	return nil
 }
