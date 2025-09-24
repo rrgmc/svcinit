e5221dc967dc7ce45883d5d9e997b33217a82fa4
Rangel Reale <rangelspam@gmail.com>
2025-09-24 10:05:13 -0300

    future

--- data.go
+++ data.go
@@ -26,25 +26,3 @@ func NewTaskFuture[T any](setupFunc TaskBuildDataSetupFunc[T], options ...TaskBu
 		future: dr,
 	}
 }
-
-type taskFuture[T any] struct {
-	*BaseOverloadedTask
-	future FutureResolver[T]
-}
-
-var _ Future[int] = (*taskFuture[int])(nil)
-var _ Task = (*taskFuture[int])(nil)
-var _ TaskSteps = (*taskFuture[int])(nil)
-var _ TaskWithOptions = (*taskFuture[int])(nil)
-
-func (t *taskFuture[T]) Run(ctx context.Context, step Step) error {
-	return t.Task.Run(ctx, step)
-}
-
-func (t *taskFuture[T]) Value(options ...FutureValueOption) (T, error) {
-	return t.future.Value(options...)
-}
-
-func (t *taskFuture[T]) Done() <-chan struct{} {
-	return t.future.Done()
-}
--- data_helpers.go
+++ data_helpers.go
@@ -1,51 +1,25 @@
 package svcinit
 
-// import "sync/atomic"
-//
-// type BaseResolvedData struct {
-// 	isResolved   atomic.Bool
-// 	resolvedChan chan struct{}
-// }
-//
-// var _ ResolvedData = (*BaseResolvedData)(nil)
-//
-// func NewBasedResolvedData() *BaseResolvedData {
-// 	return &BaseResolvedData{
-// 		resolvedChan: make(chan struct{}),
-// 	}
-// }
-//
-// func (d *BaseResolvedData) IsResolved() bool {
-// 	return d.isResolved.Load()
-// }
-//
-// func (d *BaseResolvedData) DoneResolved() <-chan struct{} {
-// 	return d.resolvedChan
-// }
-//
-// func (d *BaseResolvedData) SetResolved(options ...BaseResolvedDataSetOption) {
-// 	var optns baseResolvedDataSetOptions
-// 	for _, opt := range options {
-// 		opt(&optns)
-// 	}
-// 	if d.isResolved.CompareAndSwap(false, true) {
-// 		if optns.resolvedFunc != nil {
-// 			optns.resolvedFunc()
-// 		}
-// 		close(d.resolvedChan)
-// 	}
-// }
-//
-// type BaseResolvedDataSetOption func(*baseResolvedDataSetOptions)
-//
-// func WithResolvedFunc(resolvedFunc func()) BaseResolvedDataSetOption {
-// 	return func(opts *baseResolvedDataSetOptions) {
-// 		opts.resolvedFunc = resolvedFunc
-// 	}
-// }
-//
-// type baseResolvedDataSetOptions struct {
-// 	resolvedFunc func()
-// }
-//
-// type baseResolvedDataPrivate = BaseResolvedData
+import "context"
+
+type taskFuture[T any] struct {
+	*BaseOverloadedTask
+	future FutureResolver[T]
+}
+
+var _ Future[int] = (*taskFuture[int])(nil)
+var _ Task = (*taskFuture[int])(nil)
+var _ TaskSteps = (*taskFuture[int])(nil)
+var _ TaskWithOptions = (*taskFuture[int])(nil)
+
+func (t *taskFuture[T]) Run(ctx context.Context, step Step) error {
+	return t.Task.Run(ctx, step)
+}
+
+func (t *taskFuture[T]) Value(options ...FutureValueOption) (T, error) {
+	return t.future.Value(options...)
+}
+
+func (t *taskFuture[T]) Done() <-chan struct{} {
+	return t.future.Done()
+}
