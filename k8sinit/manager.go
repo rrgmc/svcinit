181461563c1f629688acd1dc03f76363b0c21e7f
Rangel Reale <rangelspam@gmail.com>
2025-09-28 09:50:39 -0300

    k8sinit

--- k8sinit/manager.go
+++ k8sinit/manager.go
@@ -1,6 +1,11 @@
 package k8sinit
 
-import "github.com/rrgmc/svcinit/v3"
+import (
+	"context"
+	"slices"
+
+	"github.com/rrgmc/svcinit/v3"
+)
 
 type Manager struct {
 	manager        *svcinit.Manager
@@ -13,13 +18,48 @@ func New(options ...Option) (*Manager, error) {
 		option(ret)
 	}
 
+	managerOptions := slices.Concat(ret.managerOptions,
+		[]svcinit.Option{
+			svcinit.WithStages(allStages...),
+		},
+	)
+
 	var err error
-	ret.manager, err = svcinit.New(ret.managerOptions...)
+	ret.manager, err = svcinit.New(managerOptions...)
 	if err != nil {
 		return nil, err
 	}
-
 	return ret, nil
+}
+
+// Stages returns the stages configured for execution.
+func (m *Manager) Stages() []string {
+	return m.manager.Stages()
+}
+
+func (m *Manager) IsRunning() bool {
+	return m.manager.IsRunning()
+}
+
+// AddTask add a Task to be executed at the passed stage.
+func (m *Manager) AddTask(stage string, task svcinit.Task, options ...svcinit.TaskOption) {
+	m.manager.AddTask(stage, task, options...)
+}
+
+func (m *Manager) AddTaskFunc(stage string, f svcinit.TaskFunc, options ...svcinit.TaskOption) {
+	m.AddTaskFunc(stage, f, options...)
+}
+
+func (m *Manager) AddService(stage string, service svcinit.Service, options ...svcinit.TaskOption) {
+	m.AddService(stage, service, options...)
+}
+
+func (m *Manager) Run(ctx context.Context) error {
+	return m.manager.Run(ctx)
+}
+
+func (m *Manager) RunWithStopErrors(ctx context.Context) (cause error, stopErr error) {
+	return m.manager.RunWithStopErrors(ctx)
 }
 
 type Option func(*Manager)
---
+++ k8sinit/manager_test.go
@@ -0,0 +1,64 @@
+package k8sinit
+
+import (
+	"cmp"
+	"context"
+	"sync"
+	"testing"
+	"testing/synctest"
+
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/rrgmc/svcinit/v3"
+	"gotest.tools/v3/assert"
+	cmp3 "gotest.tools/v3/assert/cmp"
+)
+
+func TestManager(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		items := &testList[string]{}
+
+		sm, err := New()
+
+		sm.AddTask(StageService, svcinit.BuildTask(
+			svcinit.WithStart(func(ctx context.Context) error {
+				items.add("start")
+				return nil
+			}),
+			svcinit.WithStop(func(ctx context.Context) error {
+				items.add("stop")
+				return nil
+			}),
+		))
+		assert.NilError(t, err)
+
+		err = sm.Run(t.Context())
+		assert.NilError(t, err)
+
+		items.assertDeepEqual(t, []string{"start", "stop"})
+	})
+}
+
+type testList[T any] struct {
+	m    sync.Mutex
+	list []T
+}
+
+func (l *testList[T]) add(item T) {
+	l.m.Lock()
+	l.list = append(l.list, item)
+	l.m.Unlock()
+}
+
+func (l *testList[T]) get() []T {
+	l.m.Lock()
+	defer l.m.Unlock()
+	return l.list
+}
+
+func (l *testList[T]) assertDeepEqual(t *testing.T, expected []T) {
+	assert.DeepEqual(t, expected, l.get(), cmpopts.SortSlices(cmp.Less[string]))
+}
+
+func (l *testList[T]) checkDeepEqual(t *testing.T, expected []T) bool {
+	return assert.Check(t, cmp3.DeepEqual(expected, l.get(), cmpopts.SortSlices(cmp.Less[string])))
+}
