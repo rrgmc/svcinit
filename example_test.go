3d29862e3650fa79102596beb303bcfb6a9b30f2
Rangel Reale <rangelspam@gmail.com>
2025-09-29 13:36:01 -0300

    simpler example

--- example_test.go
+++ example_test.go
@@ -43,9 +43,6 @@ func (s *healthService) Stop(ctx context.Context) error {
 func ExampleManager() {
 	ctx := context.Background()
 
-	// core HTTP server
-	var httpServer *http.Server
-
 	sinit, err := svcinit.New(
 		// initialization in 3 stages. Initialization is done in stage order, and shutdown in reverse stage order.
 		// all tasks added to the same stage are started/stopped in parallel.
@@ -67,45 +64,40 @@ func ExampleManager() {
 		func(ctx context.Context) (*healthService, error) {
 			return newHealthService(), nil
 		},
-		svcinit.WithDataStart(func(ctx context.Context, data *healthService) error {
-			return data.Start(ctx)
+		svcinit.WithDataStart(func(ctx context.Context, service *healthService) error {
+			return service.Start(ctx)
 		}),
-		svcinit.WithDataStop(func(ctx context.Context, data *healthService) error {
-			return data.Stop(ctx)
+		svcinit.WithDataStop(func(ctx context.Context, service *healthService) error {
+			return service.Stop(ctx)
 		}),
 	))
 
 	// add a task to start the core HTTP server.
-	sinit.
-		AddTask("service", svcinit.BuildTask(
-			svcinit.WithSetup(func(ctx context.Context) error {
-				// initialize the service in the setup step.
-				// as this may take some time in bigger services, initializing here allows other tasks to initialize
-				// at the same time.
-				httpServer = &http.Server{
-					Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-						w.WriteHeader(http.StatusOK)
-					}),
-					Addr: ":8080",
-				}
-				return nil
-			}),
-			svcinit.WithStart(func(ctx context.Context) error {
-				httpServer.BaseContext = func(net.Listener) context.Context {
-					return ctx
-				}
-				return httpServer.ListenAndServe()
-			}),
-			// stop the service. By default, the context is NOT cancelled, this method must arrange for the start
-			// function to end.
-			// Use [svcinit.WithCancelContext] if you want the context to be cancelled automatically after the
-			// first task finishes.
-			svcinit.WithStop(func(ctx context.Context) error {
-				return httpServer.Shutdown(ctx)
-			}),
-		),
-		// svcinit.WithCancelContext(true), // would cancel the "WithStart" context before calling "WithStop".
-		)
+	sinit.AddTask("service", svcinit.BuildDataTask[*http.Server](
+		func(ctx context.Context) (*http.Server, error) {
+			// initialize the service in the setup step.
+			// as this may take some time in bigger services, initializing here allows other tasks to initialize
+			// at the same time.
+			server := &http.Server{
+				Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+				Addr: ":8080",
+			}
+			return server, nil
+		},
+		svcinit.WithDataStart(func(ctx context.Context, service *http.Server) error {
+			service.BaseContext = func(net.Listener) context.Context {
+				return ctx
+			}
+			return service.ListenAndServe()
+		}),
+		// stop the service. By default, the context is NOT cancelled, this method must arrange for the start
+		// function to end.
+		svcinit.WithDataStop(func(ctx context.Context, service *http.Server) error {
+			return service.Shutdown(ctx)
+		}),
+	))
 
 	// shutdown on OS signal.
 	sinit.AddTask(svcinit.StageDefault, svcinit.SignalTask(os.Interrupt, syscall.SIGTERM))
