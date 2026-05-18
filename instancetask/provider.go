28527c23af72a970f1cc105099837ff39309e041
Rangel Reale <rangelspam@gmail.com>
2026-05-18 14:20:27 -0300

    wip

--- instancetask/provider.go
+++ instancetask/provider.go
@@ -6,6 +6,8 @@ import (
 	"github.com/rrgmc/svcinit/v3"
 )
 
+// Provider builds a task from the task returned by a callback.
+// Any step not set in the built task will be forwarded to it.
 func Provider(provider func(ctx context.Context) (svcinit.Task, error),
 	options ...BuildOption[svcinit.Task]) svcinit.TaskWithData[svcinit.Task] {
 	return Build[svcinit.Task](provider, append(options, WithParentFromSetup[svcinit.Task](true))...)
